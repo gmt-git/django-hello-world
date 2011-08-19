57b5f622cbd09abc734af2870897b9420d47b99b
gmt <gmt.more@gmail.com>
2011-08-19 19:08:01 +0300

    Add testing

--- django_hello_world/hello/tests.py
+++ django_hello_world/hello/tests.py
@@ -5,7 +5,10 @@ when you run "manage.py test".
 Replace this with more appropriate tests for your application.
 """
 
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+from django.test.client import Client
+
 
 
 class SimpleTest(TestCase):
@@ -14,3 +17,11 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+
+class HttpTest(TestCase):
+    def test_home(self):
+        c = Client()
+        response = c.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Hello!')
