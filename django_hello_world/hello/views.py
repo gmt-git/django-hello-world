b3860cc0590f42737aa7f032499e94403d62041c
gmt <gmt.more@gmail.com>
2011-08-19 19:08:46 +0300

    Add home page view

--- django_hello_world/hello/views.py
+++ django_hello_world/hello/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from annoying.decorators import render_to
+from django.contrib.auth.models import User
+
+
+@render_to('hello/home.html')
+def home(request):
+    users = User.objects.filter()
+    return {'users': users}
