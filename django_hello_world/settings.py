0e797a281fee65b069454291ce4a289297d6eeac
gmt <gmt.more@gmail.com>
2011-08-19 22:34:01 +0300

    Set ADMIN_MEDIA_PREFIX to /media/admin/

--- django_hello_world/settings.py
+++ django_hello_world/settings.py
@@ -65,7 +65,7 @@ STATIC_URL = '/static/'
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/static/admin/'
+ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
