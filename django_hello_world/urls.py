8a5ea18552e62bc3e669334d9ff7d44935a5f741
gmt <gmt.more@gmail.com>
2011-08-19 19:10:00 +0300

    Change urls.py

--- django_hello_world/urls.py
+++ django_hello_world/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'django_hello_world.views.home', name='home'),
+    url(r'^$', 'django_hello_world.hello.views.home', name='home'),
     # url(r'^django_hello_world/', include('django_hello_world.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
